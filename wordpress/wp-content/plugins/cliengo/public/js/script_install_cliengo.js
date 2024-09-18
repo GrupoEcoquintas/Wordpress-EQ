889d484e96dc9fdd30747f8e3465ffe9563f251f
Juan Miguel Fernandez <82823629+9ero@users.noreply.github.com>
2024-09-18 10:16:07 -0600

    first commit

---
+++ wordpress/wp-content/plugins/cliengo/public/js/script_install_cliengo.js
@@ -0,0 +1,1 @@
+(function(){var ldk=document.createElement("script"); ldk.type="text/javascript";ldk.async=true; ldk.src="https://s.cliengo.com/weboptimizer/60136b8fcc5440002a83c705/603e72da134fca002acc4bb7.js?platform=wordpress"; var s=document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ldk, s);})();
